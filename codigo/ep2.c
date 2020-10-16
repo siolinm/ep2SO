9c09c33117ac194af49ff5cb5d75801ea3dbcb45
Lucas Paiolla <lucaspaiolla@gmail.com>
2020-10-16 18:52:27 -0300

    Começamos o projeto e estamos pensando nas decisões de implementação

--- codigo/ep2.c
+++ codigo/ep2.c
@@ -1,6 +1,49 @@
-#include<pthread.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+#include "util.h"
 
-    return 0;
+#define MAX_CICLISTAS 500
+#define D_MAX 10000
+#define FAIXAS 10
+
+typedef struct {
+	int velocidade;     // velocidade atual
+	int i, j;           // posição na pista
+	int quando_quebrou; // -1 caso não quebrou, t caso chegou no instante t
+	int volta_atual;    // o número da volta atual
+	int chegou;         // -1 caso não chegou ou t caso chegou no instante t
+} competidor;
+
+competidor ciclistas[MAX_CICLISTAS]; // Array com todos os ciclistas
+int n_ciclistas;                     // Número de ciclistas no array
+int pista[D_MAX][FAIXAS];
+pthread_mutex_t mutex[D_MAX][FAIXAS];
+
+pthread_barrier_t barreira;
+
+int d, n; // Valores d e n do enunciado
+
+/** Função que executada por cada thread */
+void *ciclista(void *arqv) {
+	int round;
+	int max_round;
+
+
+	return NULL;
+}
+
+void mostreUso() {
+	fprintf(stderr, "Faltando parâmetros, para usar digite:\n"
+	                "./ep2 <d> <n>\n");
+}
+
+int main(int argc, char *argv[]) {
+	if (argc < 3) mostreUso();
+
+	d = atoi(argv[1]);
+	n = atoi(argv[2]);
+
+	return 0;
 }
---
+++ codigo/util.h
@@ -0,0 +1,6 @@
+#ifndef _UTIL_H
+#define _UTIL_H
+
+typedef enum { False, True } Bool;
+
+#endif /* ifndef _UTIL_H */
